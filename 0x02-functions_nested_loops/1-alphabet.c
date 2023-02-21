02efba1c1bebcb8875cf918a88cc8530eb69d9b1
DrBake <mangilenox@gmail.com>
2023-02-21 20:49:22 +0300

    funtion to print a - z

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
-/**print alphabet - utilize the putchar function from main.h to print a-z
- *
- *
- */
+/**
+ * print_alphabet - utilizes on the _putchar from main.h function to print
+ *                 the alphabet a - z
+*/
 
 void print_alphabet(void)
 {
 	int ch;
-	for (ch = 'a'; ch <= 'z'; ch++)
-		_putchar(ch)
-	_putchar('\n')
+
+	for (ch = 'a'; ch <= 'z'; ++ch)
+		_putchar(ch);
+	_putchar('\n');
 }
