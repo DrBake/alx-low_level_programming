bcde6579dae827f76912619c15d4263c82137a68
DrBake <mangilenox@gmail.com>
2023-04-18 10:09:57 +0300

    A function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,42 +1,38 @@
 
 /**
- * create_file - creates a file with the given filename and writes the
- *               given text content to it.
+ * create_file - creates a file and writes text to it
+ * @filename: the name of the file to create
+ * @text_content: the text to write to the file
  *
- * @filename: name of file to create
- * @text_content: string to write to file
- *
- * Return: 1 on success OR -1 on failure
+ * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-    int file, write_status, words = 0;
+	int file, write_status, words = 0;
 
-    /* Check if filename is present */
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL) /* check if filename is present */
+		return (-1);
 
-    /* Open file in write mode and create if it doesn't exist, else truncate */
-    file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (file == -1)
-        return (-1); /* Check if file creation was a success */
+	/* open file by creating it and if it exists write but truncate to 0 */
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (file == -1) /* check if file creation was a success */
+		return (-1);
 
-    /* Write content to file if it's not NULL */
-    if (text_content)
-    {
-        /* Find number of words in text content */
-        while (text_content[words] != '\0')
-            words++;
+	if (text_content) /* write content to file if it's not NULL */
+	{
+		/* find the number of words in the text_content string */
+		while (text_content[words] != '\0')
+			words++;
 
-        /* Write to file */
-        write_status = write(file, text_content, words);
-        if (write_status == -1)
-        {
-            close(file);
-            return (-1); /* Check if write was a success */
-        }
-    }
+		/* write text_content to file */
+		write_status = write(file, text_content, words);
+		if (write_status == -1) /* check if write was a success */
+		{
+			close(file);
+			return (-1);
+		}
+	}
 
-    close(file); /* Close file */
-    return (1);
+	close(file); /* close file */
+	return (1);
 }
