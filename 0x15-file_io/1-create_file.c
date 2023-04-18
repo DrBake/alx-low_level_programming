5b631cb42b07f99e366d9e1e754f3866fcdaf450
DrBake <mangilenox@gmail.com>
2023-04-18 09:55:15 +0300

    A function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,39 +1,42 @@
 
 /**
- * create_file - creates a file with the given name and writes the
- *               given string to it
- * @filename: the name of the file to create
- * @text_content: the string to write to the file (can be NULL)
+ * create_file - creates a file with the given filename and writes the
+ *               given text content to it.
  *
- * Return: 1 on success, -1 on failure
+ * @filename: name of file to create
+ * @text_content: string to write to file
+ *
+ * Return: 1 on success OR -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file_descriptor, write_result, num_words = 0;
+    int file, write_status, words = 0;
 
-	if (filename == NULL) /* check if filename is valid */
-		return (-1);
+    /* Check if filename is present */
+    if (filename == NULL)
+        return (-1);
 
-	/* create the file with read and write permissions for user */
-	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (file_descriptor == -1) /* check if file creation failed */
-		return (-1);
+    /* Open file in write mode and create if it doesn't exist, else truncate */
+    file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+    if (file == -1)
+        return (-1); /* Check if file creation was a success */
 
-	if (text_content != NULL) /* write content to file if it's not NULL */
-	{
-		/* count number of words in the string */
-		while (text_content[num_words] != '\0')
-			num_words++;
+    /* Write content to file if it's not NULL */
+    if (text_content)
+    {
+        /* Find number of words in text content */
+        while (text_content[words] != '\0')
+            words++;
 
-		/* write the string to the file */
-		write_result = write(file_descriptor, text_content, num_words);
-		if (write_result == -1) /* check if write failed */
-		{
-			close(file_descriptor);
-			return (-1);
-		}
-	}
+        /* Write to file */
+        write_status = write(file, text_content, words);
+        if (write_status == -1)
+        {
+            close(file);
+            return (-1); /* Check if write was a success */
+        }
+    }
 
-	close(file_descriptor); /* close the file */
-	return (1);
+    close(file); /* Close file */
+    return (1);
 }
